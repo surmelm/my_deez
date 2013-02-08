ef89a48579c20205a66762c46ac521a6bb03e80e
Nisouvendia <nisouvendia@MacBook-Pro-de-Muriel.local>
2013-02-09 00:45:00 +0100

    test

--- cakephp/app/Controller/HomeController.php
+++ cakephp/app/Controller/HomeController.php
@@ -2,14 +2,12 @@
 
 	/**
 	* L'objet Home, pour les users inscrit ou pas. 
+	* C'est l'accueil du site tous le users verrons la barre de recherche les musiques etc ..
+	* ils pourront ecouter de la musique mais que des extrait des zik ou alors que pendant 1h a voir
 	*/
 	class HomeController extends AppController
 	{
 		function index()
 		{
 		}
-		/*function admin()
-		{
-			$this->layout = "tempadmin";
-		}*/
 	}
